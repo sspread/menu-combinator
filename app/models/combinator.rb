6f8afe7309865546ad81bbd58d866acb6a0276c5
sspread <sspread2@gmail.com>
2014-12-11 01:49:17 -0600

    Cleaning up home page with partials

--- app/models/combinator.rb
+++ app/models/combinator.rb
@@ -8,54 +8,33 @@ class Combinator
     @combos = []
   end
 
-  def money_to_integer!(arr)
-    arr.each {|obj| obj.convert_price_to_integer }
-  end
-
-  def integer_to_money(hash)
-    hash.values.map {|n| n.to_f/100}
-  end
-  def self.combo_total(hash)
-    sum = []
-    hash.each do |key, value|
-      sum << key*value
-    end
-    sum.inject(:+)
-  end
-
-  def min_attribute_value_in_arr_of_objects(arr)
-    min = 0
-    arr.each do |obj|
-      price = yield obj
-      min = price if price < min
-    end
-    min
-  end
-
-  def solve(total = @total, menu_items = @menu_items, combo = {})
+  def solve(total = @total, combo = {})
     if total == 0
-      puts "Pushing combo: #{combo}"
       @combos << combo unless @combos.include? combo
       return
     end
 
-    menu_items = menu_items.select {|item| item.price <= total} 
-    menu_items.each do |item|
-      puts "First Loop"
+    @menu_items.each do |item|
       range = (1..(total/item.price))
       range.each do |quantity|
-        puts "Second Loop"
         unless combo[item]
-          combo_copy = combo.dup
+          combo_copy = combo.clone
           combo_copy[item] = quantity
           new_total = total-(item.price*quantity)
-          solve(new_total, menu_items, combo_copy)
+          solve(new_total, combo_copy)
         else
-          return
+          next
         end
       end
     end
   end
+  
+  private
+  
+  def money_to_integer!(arr)
+    arr.each {|obj| obj.convert_price_to_integer }
+  end
+
 end
 
 
